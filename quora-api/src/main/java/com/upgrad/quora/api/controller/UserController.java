b07907c005cfd8066a08f7c979509b9c808a2af2
Anuj Batwara <anuj.batwara@gmail.com>
2020-11-25 18:24:47 +0530

    first commit with signup module

---
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -0,0 +1,44 @@
+package com.upgrad.quora.api.controller;
+
+import com.upgrad.quora.api.model.SignupUserRequest;
+import com.upgrad.quora.api.model.SignupUserResponse;
+import com.upgrad.quora.service.business.SignupBusinessService;
+import com.upgrad.quora.service.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+    @Autowired
+    private SignupBusinessService signupBusinessService;
+
+    @RequestMapping(method = RequestMethod.POST, path = "/user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws ParseException {
+        final UserEntity userEntity = new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setUserName(signupUserRequest.getUserName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setMobilePhone(signupUserRequest.getContactNumber());
+        userEntity.setSalt("1234abc");
+        userEntity.setCountry(signupUserRequest.getCountry());
+        SimpleDateFormat objSDF = new SimpleDateFormat("dd-mm-yyyy");
+        userEntity.setDob(objSDF.parse("06-08-1987"));
+        userEntity.setAboutme(signupUserRequest.getAboutMe());
+        final UserEntity createdUserEntity = signupBusinessService.signup(userEntity);
+        SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid()).status("REGISTERED");
+        return new ResponseEntity<SignupUserResponse>(userResponse, HttpStatus.CREATED);
+    }
+}
